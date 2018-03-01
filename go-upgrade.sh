27263a9c08f19c24a87c815bd73c44ffaaf2c76b
CyanZee <35331346+CyanZee@users.noreply.github.com>
2018-03-01 10:37:55 +0800

    go programming environment setup.
    present go version is go1.9.4.

---
+++ go-upgrade.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get install gccgo-5
+
+cd ~/
+#export http_proxy=192.168.152.1:42581
+#export https_proxy=192.168.152.1:42581
+#export sock5_proxy=192.168.152.1:42581
+rm -rf go go1.4
+
+wget -c https://storage.googleapis.com/golang/go1.4-bootstrap-20161024.tar.gz
+tar -zxf go1.4-bootstrap-20161024.tar.gz
+mv go go1.4
+cd ~/go1.4/src
+sudo update-alternatives --set go /usr/bin/go-5
+GOROOT_BOOTSTRAP=/usr ./make.bash
+cd ~
+#git clone https://github.com/golang/go.git
+#wget https://storage.googleapis.com/golang/go1.8.src.tar.gz
+#wget -c -O https://codeload.github.com/golang/go/tar.gz/go1.9.4
+wget -c https://storage.googleapis.com/golang/go1.9.4.src.tar.gz
+
+tar -zxf go1.9.4.src.tar.gz
+cd go/src
+./all.bash
